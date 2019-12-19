85a65b34764765b95ab8c132419b39825facb8b9
moonfox <sleepmoon.jia@gmail.com>
2019-12-19 23:48:16 +0800

    fix segmentation fault bug

--- c/two-sum.c
+++ c/two-sum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int *twoSum(int *nums, int numsSize, int target, int *returnSize);
 void print_ary(int ary[], int n);
@@ -8,9 +9,9 @@ int main(int argc, char const *argv[])
 {
   int nums[] = {16, 2, 7, 11, 15};
   int nums_size = 5;
-  int *resault, *returnSize;
+  int *resault, returnSize = 0;
 
-  resault = twoSum(nums, nums_size, 9, returnSize);
+  resault = twoSum(nums, nums_size, 9, &returnSize);
   print_ary(resault, 2);
   free(resault);
   return 0;
@@ -18,6 +19,8 @@ int main(int argc, char const *argv[])
 
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
+  // 不能直接写多少字节是因为int型占用字节数由机器决定，所以要使用sizeof得到当前机器int
+  // 所分配的字节数。int最少为16字节
   int *result = (int *)malloc(sizeof(int) * 2);
 
   for (int i = 0; i < numsSize; i++)
